4d00fb91187891fe5e3dc7ccaba2808300421e61
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-10-16 23:56:27 +0100

    Mapping Collections

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -5,6 +5,7 @@ using angular_dotnet.Models;
 using angular_dotnet.Persistence;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace angular_dotnet.Controllers
 {
@@ -18,12 +19,34 @@ namespace angular_dotnet.Controllers
             this.context = context;
             this.mapper = mapper;
         }
+
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.last_update = DateTime.Now;
             context.vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+var vehicle = await context.vehicles.Include(v => v.features ).SingleOrDefaultAsync(v => v.id == id);            
+mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.last_update = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -25,14 +25,23 @@ namespace angular_dotnet.Mapping
 
             //API Resource to Domain
             CreateMap<VehicleResource, Vehicle>()
+            .ForMember(v => v.id, opt => opt.Ignore())
             .ForMember(v => v.contact_name, opt => opt.MapFrom(vr => vr.contact.name))
             .ForMember(v => v.contact_email, opt => opt.MapFrom(vr => vr.contact.email))
             .ForMember(v => v.contact_phone, opt => opt.MapFrom(vr => vr.contact.phone))
-            .ForMember(v => v.features, 
-            opt => opt.MapFrom(vr => 
-            vr.features.Select(id => 
-            new VehicleFeature { featureid = id})))
-            ;
+            .ForMember(v => v.features, opt => opt.Ignore())
+            
+            .AfterMap((vr, v) => {
+                //Remove unselected feature
+                var removedFeatures = v.features.Where(f => !vr.features.Contains(f.featureid));
+                foreach (var f in removedFeatures)
+                v.features.Remove(f);
+
+            // Add new features
+                var addedFeatures = vr.features.Where(id => !v.features.Any(f => f.featureid == id)).Select(id => new VehicleFeature { featureid = id });   
+                foreach (var f in addedFeatures)
+                v.features.Add(f);
+            });
         }
     }
 }
